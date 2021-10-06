00a4bea86424bd4085027ac6d6d9ac6a12d25de7
SATISH <SATISH@LAPTOP-HBOSBE0Q>
2021-10-06 19:51:20 +0530

    updated by satish

--- GitProject/src/com/cjc/DemoGit.java
+++ GitProject/src/com/cjc/DemoGit.java
@@ -7,7 +7,6 @@ public class DemoGit
 		
 		System.out.println("Git project");
 		System.out.println("New Change-Puja Dhavan ");
-		System.out.println("I am Rakesh Patil");
-		
+		System.out.println("I am Rakesh Patil");	
 	}
 }
