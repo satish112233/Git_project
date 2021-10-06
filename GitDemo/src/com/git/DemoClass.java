f64b935ba0f4af9299783f343159416c0b30bd4a
satish112233 <91933977+satish112233@users.noreply.github.com>
2021-10-06 13:28:27 +0530

    update by satish

--- GitDemo/src/com/git/DemoClass.java
+++ GitDemo/src/com/git/DemoClass.java
@@ -7,6 +7,11 @@ public class DemoClass
 	{
 		System.out.println("this is m1 method");
 	}
+	
+	public void branch()
+	{
+		System.out.println("Branching code");
+	}
 	public static void main(String[] args) {
 		
 		System.out.println("Git first project");
