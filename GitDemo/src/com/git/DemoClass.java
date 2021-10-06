b210f5382c1d339aaf21a108dda9dee853d137f4
satish112233 <91933977+satish112233@users.noreply.github.com>
2021-10-06 10:26:33 +0530

    updated by satish added m1 method

--- GitDemo/src/com/git/DemoClass.java
+++ GitDemo/src/com/git/DemoClass.java
@@ -3,6 +3,10 @@ package com.git;
 public class DemoClass 
 {
 
+	public void m1()
+	{
+		System.out.println("this is m1 method");
+	}
 	public static void main(String[] args) {
 		
 		System.out.println("Git first project");
