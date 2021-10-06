2ce1606e9203dbbc3b97d347567f7ce90de05f9f
amarbele <91940704+amarbele@users.noreply.github.com>
2021-10-06 22:27:08 +0530

    Update Demo.java

--- GitProjectDemo/src/com/cjc/Demo.java
+++ GitProjectDemo/src/com/cjc/Demo.java
@@ -3,6 +3,10 @@ package com.cjc;
 public class Demo 
 {
 
+	public void m1()
+	{
+		System.out.println("hello m1");
+	}
 	public static void main(String[] args) {
 		
 		System.out.println("hello");
