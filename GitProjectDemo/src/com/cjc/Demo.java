fd298ef818029c15ffcaeb192f6ae3a813aa0cba
amarbele <91940704+amarbele@users.noreply.github.com>
2021-10-07 15:41:20 +0530

    updated
    by amar

--- GitProjectDemo/src/com/cjc/Demo.java
+++ GitProjectDemo/src/com/cjc/Demo.java
@@ -3,6 +3,11 @@ package com.cjc;
 public class Demo 
 {
 
+	public void m1()
+	{
+		System.out.println("hello this is m1 method");
+	}
+	
 	public static void main(String[] args) {
 		
 		System.out.println("hello");
